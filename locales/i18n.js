2bc6485d56ad9971ee696207164e0551cfe0ab1c
Simen Abelsen <simenabelsen@hotmail.no>
2021-09-20 21:21:43 +0200

    feat(i18n): Try loading i18n files asynchronously to fix opera bugs

--- locales/i18n.js
+++ locales/i18n.js
@@ -1,8 +1,14 @@
-import { addMessages, locale, getLocaleFromNavigator } from 'svelte-i18n'
+import { addMessages, register, init } from 'svelte-i18n'
 import en from './en.json'
 import nb from './nb.json'
 
 addMessages('en', en)
 addMessages('nb', nb)
 
-locale.set(getLocaleFromNavigator() ?? 'en')
+register('en', () => import('./.en.json'))
+register('nb', () => import('./nb.json'))
+
+init({
+  fallbackLocale: 'nb',
+  initialLocale: 'nb'
+})
